cc8236ad5012881f4ab564e7a3c7f68d906ee663
David <d.robles1297@Yahoo.com>
2024-02-22 21:18:31 -0600

    added logout stuff

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,76 +3,33 @@ const Deck = require('../models/Deck');
 const User = require('../models/User');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-    try {
-      // Get all decks
-      const decksData = await Deck.findAll({
-        attributes: ['id', 'name'],
-      });
-  
-      // Serialize data
-      const decks = decksData.map((deck) => deck.get({ plain: true }));
-  
-      // Pass serialized data and session flag into template
-      res.render('homepage', { 
-        decks, 
-        logged_in: req.session.logged_in 
-      });
-    } catch (err) {
-      res.status(500).json(err);
+router.get('/', async (req, res) => {
+  try {
+    // Check if the user is logged in
+    if (!req.session.logged_in) {
+      // If not logged in, redirect to the login page
+      return res.redirect('/login');
     }
+
+    // Get all decks
+    const decksData = await Deck.findAll({
+      attributes: ['id', 'name'],
+    });
+
+    // Serialize data
+    const decks = decksData.map((deck) => deck.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      decks, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// router.post('/', async (req, res) => {
-//     try {
-//       const userData = await User.create(req.body);
-  
-//       req.session.save(() => {
-//         req.session.user_id = userData.id;
-//         req.session.logged_in = true;
-  
-//         res.status(200).json(userData);
-//       });
-//     } catch (err) {
-//       res.status(400).json(err);
-//     }
-//   });
-
-
-
-  
-  // router.post('/login', async (req, res) => {
-  //     try {
-  //         const userData = await User.findOne({ where: { email: req.body.email } });
-          
-  //         if (!userData) {
-  //             res
-  //             .status(400)
-  //             .json({ message: 'Incorrect email or password, please try again' });
-  //             return;
-  //           }
-            
-  //           const validPassword = await userData.checkPassword(req.body.password);
-            
-  //           if (!validPassword) {
-  //               res
-  //               .status(400)
-  //               .json({ message: 'Incorrect email or password, please try again' });
-  //               return;
-  //           }
-            
-  //           req.session.save(() => {
-  //               req.session.user_id = userData.id;
-  //               req.session.logged_in = true;
-                
-  //               res.json({ user: userData, message: 'You are now logged in!' });
-  //           });
-            
-  //       } catch (err) {
-  //           res.status(400).json(err);
-  //       }
-  //   });
-    
+   
     // Use withAuth middleware to prevent access to route
     router.get('/flashcardedit', withAuth, async (req, res) => {
         try {
@@ -112,5 +69,17 @@ router.get('/', withAuth, async (req, res) => {
         res.render('login');
       });
       
+      router.get('/logout', (req, res) => {
+        req.session.destroy((err) => {
+          if (err) {
+            console.error('Error destroying session:', err);
+            res.status(500).send('Error logging out');
+          } else {
+            // Redirect to the login page after session is destroyed
+            res.redirect('/login');
+          }
+        });
+      });
+
     module.exports = router;
     
